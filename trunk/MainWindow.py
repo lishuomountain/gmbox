5744064a136bcb1e8a68a4592edba963141ea7e6
bones7456 <bones7456@df72c834-2380-11de-9d52-39b120432c5d>
2009-04-12 12:07:59 +0000

    UI完善中

--- trunk/MainWindow.py
+++ trunk/MainWindow.py
@@ -1,19 +1,95 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
-import gtk
+import os,gtk
+import gmbox
 
-
+(COL_NUM, COL_TITLE,
+ COL_ARTIST, COL_ALBUM,
+ COL_SIZE, COL_URL) = range(6)
 class MainWindow(gtk.Window):
     def __init__(self, parent=None):
         gtk.Window.__init__(self)
 
         self.connect('destroy', gtk.main_quit)      
+        
+        vbox = gtk.VBox()
+        vbox.set_border_width(10)
+        self.add(vbox)
+        
+        hbox = gtk.HBox()
+        #label = gtk.Label('/'.join(
+        #['%s'%key for key in gmbox.songlists]))
+        #vbox.pack_start(label, False)
+        opt = gtk.OptionMenu()
+        menu = gtk.Menu()
+        for slist in gmbox.songlists:
+            menu.append(gtk.MenuItem(slist))
+        opt.set_menu(menu)
+        hbox.pack_start(opt, False)
+        button = gtk.Button('获取列表')
+        size = button.size_request()
+        button.set_size_request(size[0]+50, -1)
+        opt.set_size_request(size[0]+150, -1)
+        #button.connect('clicked', self.doSearch, None)
+        hbox.pack_start(button, False)
+        vbox.pack_start(hbox, False)
+        
+        scroll = gtk.ScrolledWindow()
+        scroll.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        scroll.set_shadow_type(gtk.SHADOW_ETCHED_IN)
+        tree = self.setTreeView()
+        tree.set_rules_hint(True)
+        scroll.add(tree)
+        vbox.pack_start(scroll)
+        
         self.set_title('GMBox')
         self.set_default_size(800, 600)
+        
+        
+    def setTreeView(self):
+        #依次存入：歌曲编号，歌曲名，歌手，专辑，长度，url
+        self.model = gtk.ListStore(str, str, str, str, str, str)
+        treeview = gtk.TreeView(self.model)
+        #treeview.connect('button-press-event', self.onSearchListRightClicked, None)
+        treeview.get_selection().set_mode(gtk.SELECTION_SINGLE)
+        
+        renderer = gtk.CellRendererText()
+        renderer.set_data("column", COL_NUM)
+        column = gtk.TreeViewColumn("编号", renderer, text=COL_NUM)
+        column.set_resizable(True)
+        treeview.append_column(column)
 
+        renderer = gtk.CellRendererText()
+        renderer.set_data("column", COL_TITLE)
+        renderer.set_property('editable', True)
+        #renderer.connect("edited", self.on_cell_edited, None)
+        column = gtk.TreeViewColumn("歌曲", renderer, text=COL_TITLE)
+        column.set_resizable(True)
+        treeview.append_column(column)
 
+        renderer = gtk.CellRendererText()
+        renderer.set_data("column", COL_ARTIST)
+        renderer.set_property('editable', True)
+        #renderer.connect("edited", self.on_cell_edited, None)
+        column = gtk.TreeViewColumn("歌手", renderer, text=COL_ARTIST)
+        column.set_resizable(True)
+        treeview.append_column(column)
+
+        renderer = gtk.CellRendererText()
+        renderer.set_data("column", COL_ALBUM)
+        renderer.set_property('editable', True)
+        #renderer.connect("edited", self.on_cell_edited, None)
+        column = gtk.TreeViewColumn("专辑", renderer, text=COL_ALBUM)
+        column.set_resizable(True)
+        treeview.append_column(column)
+
+        renderer = gtk.CellRendererText()
+        renderer.set_data("column", COL_SIZE)
+        column = gtk.TreeViewColumn("长度", renderer, text=COL_SIZE)
+        column.set_resizable(True)
+        treeview.append_column(column)
+        return treeview
 
 def main():
     win = MainWindow();
