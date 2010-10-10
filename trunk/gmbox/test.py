ed241b6960ee6a55c3e870e235f29c00ccba6814
muzuiget <muzuiget@df72c834-2380-11de-9d52-39b120432c5d>
2010-10-10 00:58:32 +0000

    文件名模版加入音轨序号占位符

--- trunk/gmbox/test.py
+++ trunk/gmbox/test.py
@@ -29,7 +29,7 @@ def print_directory(directory):
 if __name__ == '__main__':
     
 #    print "%s\n" % Song.musicdownload("S5c956b9af4dc56ba")
-#    print_song(Song("Sb1ee641ab6133e1a"))
+    print_song(Song("Sb1ee641ab6133e1a"))
     
 #    print_songlist(Album("B5f03f5ad567ecbec"))
 #    print_songlist(Search("beyond"))
@@ -38,7 +38,7 @@ if __name__ == '__main__':
 #    print_songlist(ArtistSong("A887b2d5bdd631594"))
 #    print_songlist(Tag("%E6%82%A0%E6%89%AC"))
 #    print_songlist(Screener())
-    print_songlist(Similar("Sb1ee641ab6133e1a"))
+#    print_songlist(Similar("Sb1ee641ab6133e1a"))
 #    print_songlist(Starrecc("top100_star_chenyixun"))
 #
 #    print_directory(DirSearch("海阔天空"))
