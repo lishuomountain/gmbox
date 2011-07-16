78f8da3b886d33d5c81203057da590aab301a997
muzuiget <muzuiget@df72c834-2380-11de-9d52-39b120432c5d>
2011-07-16 05:30:30 +0000

    调用外部播放器时预先建立下载目录

--- gmbox-gtk/gmbox/config.py
+++ gmbox-gtk/gmbox/config.py
@@ -8,33 +8,33 @@ import glib
 
 def get_program_root_path():
     '''获取程序根目录地址
-    
+
     在win下用py2exe打包后，跟直接运行py文件，程序地址有所不同
     '''
-    
+
     if hasattr(sys, "frozen"):
         program_file_path = unicode(sys.executable, sys.getfilesystemencoding())
     else:
         program_file_path = unicode(os.path.abspath(__file__), sys.getfilesystemencoding())
-	program_root_path = os.path.dirname(os.path.dirname(program_file_path))
+    program_root_path = os.path.dirname(os.path.dirname(program_file_path))
     return program_root_path
 
 PROGRAM_ROOT_PATH = get_program_root_path()
 
 def get_glade_file_path(filename):
-	""" 获得glade文件路径"""
-	glade_folder_path = "%s/data/glade" % PROGRAM_ROOT_PATH
-	return "%s/%s" % (glade_folder_path,  filename)
+    """ 获得glade文件路径"""
+    glade_folder_path = "%s/data/glade" % PROGRAM_ROOT_PATH
+    return "%s/%s" % (glade_folder_path,  filename)
 
 def get_pixbuf_file_path(filename):
-	""" 获得pixbuf文件路径"""
-	pixbuf_folder_path = "%s/data/pixbufs" % PROGRAM_ROOT_PATH
-	return "%s/%s" % (pixbuf_folder_path,  filename)
+    """ 获得pixbuf文件路径"""
+    pixbuf_folder_path = "%s/data/pixbufs" % PROGRAM_ROOT_PATH
+    return "%s/%s" % (pixbuf_folder_path,  filename)
 
 def create_icon_dict():
     '''创建图标集'''
     global PROGRAM_ROOT_PATH
-    
+
     icon_names = ["gmbox", "song", "songlist", "directory", "refresh", "info"]
     icon_dict = {}
     for name in icon_names:
@@ -48,7 +48,7 @@ ICON_DICT = create_icon_dict()
 
 def get_default_player():
     '''获取默认播放器路径'''
-    
+
     if sys.platform == "win32":
         return "C:\\Program Files\\Windows Media Player\\wmplayer.exe";
     else:
@@ -63,7 +63,7 @@ def get_default_player():
 
 def get_download_folder():
     '''获取下载文件夹'''
-    
+
     download_folder = glib.get_user_special_dir(glib.USER_DIRECTORY_MUSIC)
     if download_folder is None:
         download_folder = os.path.expanduser("~/Music")
@@ -85,13 +85,14 @@ CONFIG = {
     # 下载程序
     "downloader_use_internal" : True,
     "downloader_path" : "",
-    "downloader_single" : "${URL}",
+    "downloader_single" : "${URL} -O ${FILEPATH}",
     "downloader_multi" : "${URLS}",
+    "downloader_mkdir" : True,
 }
 
 def get_config_folder():
     '''获取设置文件夹'''
-    
+
     config_folder = "%s/gmbox" % glib.get_user_config_dir()
     if config_folder is None:
         config_folder = "%s/config/" % PROGRAM_ROOT_PATH
@@ -101,32 +102,32 @@ CONFIG_FOLDER = get_config_folder()
 
 def load_config_file():
     '''读取配置文件'''
-    
+
     config_file_path = "%s/gmbox.conf" % CONFIG_FOLDER
 
     if not os.path.exists(config_file_path):
         return
-    
-    config_file = open(config_file_path) 
+
+    config_file = open(config_file_path)
     text = config_file.read()
     lines = text.split("\n")
     for line in lines:
         line = line.strip()
         if line == "":
-            continue                
+            continue
         key, value = line.split("=", 1)
         if value in ["True", "true", "yes", "1"]:
             value = True
         if value in ["False", "false", "no", "0"]:
             value = False
-        CONFIG[key] = value   
+        CONFIG[key] = value
 
 def save_config_file():
     '''保存配置文件'''
-    
+
     if not os.path.exists(CONFIG_FOLDER):
         os.mkdir(CONFIG_FOLDER)
-        
+
     config_file_path = "%s/gmbox.conf" % CONFIG_FOLDER
     config_file = open(config_file_path, "w")
     for key, value in CONFIG.items():
