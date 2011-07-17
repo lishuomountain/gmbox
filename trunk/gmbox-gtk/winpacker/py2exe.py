e5028e5f99bd34f3dc4d93695358eee5c2b672f2
muzuiget <muzuiget@df72c834-2380-11de-9d52-39b120432c5d>
2011-07-17 04:26:59 +0000

    更新py2exe打包文件

--- trunk/gmbox-gtk/winpacker/py2exe.py
+++ trunk/gmbox-gtk/winpacker/py2exe.py
@@ -9,32 +9,33 @@ from distutils.core import setup
 import py2exe
 
 setup(
-    name = 'gmbox',
-    description = 'Google Music Box',
+    name = 'gmbox-gtk',
+    description = 'Google Music Box GTK',
     version = '0.4',
     windows = [
         {
-            'script':'gmbox.py',
-            'icon_resources':[(1, 'pixbufs/gmbox.ico')],
+            'script':'gmbox-gtk.py',
+            'icon_resources':[(1, 'data/pixbufs/gmbox.ico')],
         }
     ],
     options = {
         'py2exe': {
             'packages' : 'encodings',
             'includes' : 'cairo, pango, pangocairo, atk, gobject, gio',
-            'dist_dir' : 'gmbox-win',
+            'dist_dir' : 'dist',
         }
     },
     data_files=[
-        'gmbox.glade',
-        ('pixbufs',[
-            'pixbufs/directory.png',
-            'pixbufs/gmbox.ico',
-            'pixbufs/info.png',
-            'pixbufs/songlist.png',
-            'pixbufs/error.png',
-            'pixbufs/gmbox.png',
-            'pixbufs/refresh.png',
-            'pixbufs/song.png'])
-        ]
+        ('data', [
+            'data/glade/gmbox.glade',
+            'data/pixbufs/directory.png',
+            'data/pixbufs/gmbox.ico',
+            'data/pixbufs/info.png',
+            'data/pixbufs/songlist.png',
+            'data/pixbufs/error.png',
+            'data/pixbufs/gmbox.png',
+            'data/pixbufs/refresh.png',
+            'data/pixbufs/song.png'
+        ])
+    ]
 )
